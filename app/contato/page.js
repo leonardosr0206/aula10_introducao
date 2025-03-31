a7f8868cf4056ce5abdb0f85fd6984b27ad54259
leonardosr0206 <leodosanstos212325@gmail.com>
2025-03-30 22:09:29 -0300

    Update page.js

--- app/contato/page.js
+++ app/contato/page.js
@@ -1,67 +1,163 @@
-"use client"
-
-import React from 'react';
-import { useState } from "react"
-import { ToastContainer, toast } from 'react-toastify';
-
-// testando um teste com uma testada
-export default function Contato(){
-
-    const[ nome, setNome ] = useState("")
-    const[ mensagem, setMensagem ] = useState("")
-
-    const erro = () => toast.error("erro");
-    const acerto = () => toast.acerto("acerto");
-    
-
-function enviaMensagem(e){
-
-        e.preventDefault()
-
-    if ( nome == "" || mensagem == ""  ){
-        console.log("por favor envie de novo")
-       erro()
-    }else{
-        console.log(" Mensagem enviada com sucesso! ")
-        acerto()
-    }
-}
 
 
-    return(
+
+    'use client'
+
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './pagina_estoque.css';
+import { handleClientScriptLoad } from 'next/script';
+
+const Estoque = () => {
+  const [A1, alteraA1] = useState(false);
+  const [nomeProduto, alteraNomeProduto] = useState('');
+  const [precoProduto, alteraPrecoProduto] = useState('');
+  const [quantidadeProduto, alteraQuantidadeProduto] = useState('');
+  const [produtos, setProdutos] = useState([
+    { nome: 'Laranja', preco: 'R$21,00', quantidade: '100 KG' },
+    { nome: 'Mandioca', preco: 'R$0,65', quantidade: '200 KG' },
+    { nome: 'Maracuja', preco: 'R$2,89', quantidade: '50 KG' },
+  ]);
+
+  const handleClick = () => {
+    alteraA1(!A1);
+  };
+
+  const handleSalvar = () => {
+    const novoProduto = {
+      nome: nomeProduto,
+      preco: precoProduto,
+      quantidade: quantidadeProduto,
+    };
+    setProdutos([...produtos, novoProduto]);
+
+    // Limpar os campos após salvar
+    alteraNomeProduto('');
+    alteraPrecoProduto('');
+    alteraQuantidadeProduto('');
+    alteraA1(false); // Fecha o formulário após salvar
+  };
+
+  return (
     <div>
-        
-        <h1>Pagina de Contato </h1>
-        <p>Entre em contato conosco.</p>
+      <div className="menuSuperior">
+        <img className="logo" src="logo.png" />
+      </div>
 
-        <hr/><br/>
+      <div className="paineis">
+        <div className="painelEsquerdo">
+          <div className="CardGeral">
+            <div className="atualizar">
+              <button className="button" onClick={handleClick}>
+                <i className="fa-solid fa-download"></i>
+                <p>Cadastrar novo produto</p>
+              </button>
+            </div>
+          </div>
 
-        <form onSubmit={ (e)=> enviaMensagem (e) }>
+          {A1 && (
+            <>
+              <div className="Conteudo">
+                <div className="CardGeral">
+                  <input
+                    type="text"
+                    placeholder="Nome do produto"
+                    value={nomeProduto}
+                    onChange={(e) => alteraNomeProduto(e.target.value)}
+                  />
+                </div>
+              </div>
 
-        
+              <div className="Conteudo">
+                <div className="CardGeral">
+                  <input
+                    type="text"
+                    placeholder="Preço por KG"
+                    value={precoProduto}
+                    onChange={(e) => alteraPrecoProduto(e.target.value)}
+                  />
+                </div>
+              </div>
 
-                <label>
-                    Digite seu nome:
-                    <input onChange={ (e)=> setNome (e.target.value) }/>
-                </label>
+              <div className="Conteudo">
+                <div className="CardGeral">
+                  <input
+                    type="text"
+                    placeholder="Quantidade"
+                    value={quantidadeProduto}
+                    onChange={(e) => alteraQuantidadeProduto(e.target.value)}
+                  />
+                </div>
+              </div>
 
-                <br/><br/>
+              <div>
+                <div className="CardGeral">
+                  <button className="button" onClick={handleSalvar}>
+                    Salvar
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
+        </div>
 
-                <label>
-                    Digite sua mensagem:
-                    <input onChange={ (e)=> setMensagem (e.target.value) }/>
-                </label>
+        {/* Container para a tabela e o título */}
+        <div className="produtosCadastradosContainer">
+          <div className="produtosCadastradosTitulo">
+            <i className="fa-solid fa-file"></i>
+            <p>Produtos Cadastrados:</p>
+          </div>
 
-                <br/><br/>
-                
-                
-        <button >enviar</button>
-        
-        
-      
-      
-      
-        </form>
+          <div className="tabela-scroll">
+            <table className="table table-striped">
+              <thead>
+                <tr>
+                  <th scope="col">Produtos</th>
+                  <th scope="col">Preço KG</th>
+                  <th scope="col">Quantidade Disponível</th>
+                  <th scope="col">Ações</th>
+                </tr>
+              </thead>
+              <tbody>
+                {produtos.map((produto, index) => (
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{produto.nome}</td>
+                    <td>{produto.preco}</td>
+                    <td>{produto.quantidade}</td>
+                    <td>
+                      <button className="button-edit">
+                        <i className="fa-solid fa-pencil"></i>
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
+      <div className="Voltar">
+        <a href="http://localhost:3000/">
+          <button className="voltar">
+            <p>Voltar</p>
+          </button>
+        </a>
+      </div>
     </div>
-        )
+  );
+};
+
+export default Estoque;
+
+
+
+
+
+
+/* Adicionar ao css*/
+.tabela-scroll {
+  max-height: 400px; /* Tamanho máximo da tabela */
+  overflow-y: auto;  /* Permite rolar verticalmente */
 }
